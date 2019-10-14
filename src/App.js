94b2293c662d4125493305db3f8abef238ee9d61
Daniel Pali <danielpali@daniels-mbp.home>
2019-10-14 12:47:50 -0700

    'sally"

--- src/App.js
+++ src/App.js
@@ -4,23 +4,67 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+
+	<body>
+    <main role="main">
+		<header>
+			<div class="hed">
+				<h1>Sally Student</h1>
+			</div>
+		</header>
+
+
+		
+			<section>
+				<header>
+					<h2>Education:</h2>
+				</header>
+
+				<ul>
+					<li>Harvard</li>
+					<li>Yale</li>
+				</ul>
+			</section>
+			<section>
+				<header>
+					<h2>Employment History:</h2>
+				</header>
+				<section>
+					<p>2) Google</p>
+					<p>Manager</p>
+					<p>Dates of Employment: June 2106-June 2018</p>
+					<p>Job Description:</p>
+
+							<ul>
+								<li>Managed over 30 employees</li>
+								<li>Increased overall revenue by 10%</li>
+							</ul>
+				</section>
+				<section>
+					<p>Bird</p>
+          <ol>
+					<p>Sales Manager</p>
+					<p>Dates of employment June: 2014-June2016</p>
+
+					<p>Job Description:</p>
+					<ul>
+						<li>Managed a group of 10 employees</li>
+						<li>Facilitated financing program</li>
+					</ul>
+          </ol>
+				</section>
+		      </section>
+      </main>
+      <address>
+	    <p>Contact Info:</p>
+			  <a href="mailto:sallystudent@thinkful.com">sallystudent@thinkful.com</a>
+			  <p>(567)-245-2638</p>
+    </address><img src="https://m.media-amazon.com/images/M/MV5BMTY5NzE3NzU3MF5BMl5BanBnXkFtZTgwMjg0NTQ5MDE@._V1_.jpg" alt="ready for my career" />
+	</body>
+    
+   
+   );
 }
 
 export default App;
